58912a9e32cd430ea9181fa766f74301949aff28
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-01-03 22:23:46 +0100

    protected route using jwt

--- Endpoints/Authentication/Routes.js
+++ Endpoints/Authentication/Routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const route = express.Router()
+const verify = require("../../middleware/verifyJwt")
 const {signupController, loginController, userController} = require('./Controllers')
 
-route.get("/users", userController)
+route.get("/users", verify, userController)
 route.post("/signup", signupController)
 route.post("/login", loginController)
 
