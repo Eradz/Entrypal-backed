cbbfe57acdadd3757ad1e764b470d6e29c7aca9d
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-09-09 19:48:47 +0100

    Added welcome message to google auth

--- utils/googleAuthenticate.js
+++ utils/googleAuthenticate.js
@@ -1,6 +1,8 @@
 const passport = require("passport")
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const EventGoer = require("../Models/eventGoersSchema")
+const sendEmail = require("../utils/sendEmail")
+const paths = require('path')
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -15,6 +17,7 @@ passport.use(new GoogleStrategy({
          return  done(null, existingUseremail)
         } else{
           const user = await EventGoer.create({fullname: displayName, googleId: id, email, username: given_name, verified: true })
+          sendEmail(email, displayName , "Welcome to Ticketing made easy", '',  paths.join( __dirname, "../views/EventGoerSignup.ejs"))
           return done(null, user)
         }
   }
